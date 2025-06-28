881456035a47f22964aebb949f22c9af491da992
Jeffrey Weidner <jeff@qualsci.com>
2025-06-28 14:26:40 -0400

    added liscence and documentation for the first function stringsplit1

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,14 @@
+#' Split a string into a vector
+#'
+#' @param x A character vector with only one element.
+#' @param split What to split the string by. Default is a space.
+#'
+#' @returns A character vector containing the split elements.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
